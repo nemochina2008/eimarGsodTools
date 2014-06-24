58dab67f92ddf30bac9e0d27863880ce1b2cca37
fdetsch <florian.detsch@staff.uni-marburg.de>
2014-06-24 17:29:09 +0200

    Update: implemented plotting of several periods in seasonalShift.

--- R/gsodPlot.R
+++ R/gsodPlot.R
@@ -112,6 +112,36 @@ gsodPlot <- function(fls.orig = NULL,
               panel.grid.major = element_line(size = 1.2), 
               panel.grid.minor = element_line(size = 1.1))
       
+    } else if (type == "all.in.one") {
+      # Reformat and append gap-filled station data
+      ta.gf.df <- foreach(i = ta.gf, j = stations, .combine = "rbind") %do%
+        data.frame(DATE = time(i), PLOT = j, Imputed = as.numeric(i[, prm.orig]))
+      
+      # Merge and meltoriginal and gap-filled data
+      ta.orig.gf.df <- merge(ta.orig.df, ta.gf.df, all = TRUE, by = c(1, 2))
+      ta.orig.gf.df.mlt <- melt(ta.orig.gf.df, id.vars = c(1, 2))
+      
+      ta.orig.gf.df.mlt$variable <- factor(ta.orig.gf.df.mlt$variable, 
+                                           levels = c("Imputed", "Original"))
+      
+      ggplot(aes(x = DATE, y = value, group = variable, colour = variable), 
+             data = ta.orig.gf.df.mlt) + 
+        geom_line() + 
+        facet_wrap(~ PLOT, ncol = 2) + 
+        scale_x_date(limits = c(as.Date("1973-01-01"), as.Date("2013-12-31")), 
+                     breaks = seq(as.Date("1970-01-01"), as.Date("2020-01-01"), "10 years"), 
+                     labels = date_format("%Y"), minor_breaks = date_breaks("2 years")) + 
+        scale_colour_manual("", values = c("grey65", "black"), 
+                            labels = c("Original data", "Imputed data"), 
+                            breaks = c("Original", "Imputed")) +
+        labs(list(title = "GSOD daily air temperature (1973-2012)\n", 
+                  x = "\nTime [d]", y = "Temperature [°C]\n")) + 
+        theme_bw() + 
+        theme(text = element_text(...), 
+              legend.key = element_rect(fill = "transparent"), 
+              panel.grid.major = element_line(size = 1.2), 
+              panel.grid.minor = element_line(size = 1.1))
+      
       # Plot gap-filled GSOD data only  
     } else {  
       # Reformat and append gap-filled data
--- R/seasonalShift.R
+++ R/seasonalShift.R
@@ -5,6 +5,8 @@ seasonalShift <- function(fls,
                           start = c("1973-01-01", "1977-12-31"),
                           end = c("2008-01-01", "2012-12-31"), 
                           stations,
+                          ncol = 2,
+                          palette = c("cornflowerblue", "red2", "grey65", "grey35"), 
                           ...) {
   
   # Required packages
@@ -22,49 +24,92 @@ seasonalShift <- function(fls,
   # Import data
   tmp.all <- foreach(i = fls, j = stations, .combine = "rbind") %do% {
     
-    tmp <- read.csv(i, stringsAsFactors = FALSE)
-    tmp$year <- as.Date(substr(tmp$Datetime, 1, 10))
-    tmp$yearmon <- as.yearmon(tmp$year)
-    
-    tmp.st <- subset(tmp, year >= as.Date(start[1]) & year <= as.Date(start[2]))
-    tmp.st.agg <- aggregate(tmp.st[, c("TEMP", "MAX", "MIN")], 
-                                   by = list(tmp.st$yearmon), FUN = mean)
-    
-    tmp.nd <- subset(tmp, year >= as.Date(end[1]) & year <= as.Date(end[2]))
-    tmp.nd.agg <- aggregate(tmp.nd[, c("TEMP", "MAX", "MIN")], 
-                                   by = list(tmp.nd$yearmon), FUN = mean)
-    
-    tst.nd <- vectorHarmonics(tmp.nd.agg$TEMP, frq = 12, fun = mean, m = 2,
-                              st = c(as.numeric(substr(start[1], 1, 4)), 01), 
-                              nd = c(as.numeric(substr(start[2], 1, 4)), 12))
-    tst.st <- vectorHarmonics(tmp.st.agg$TEMP, frq = 12, fun = mean, m = 2,
-                              st = c(as.numeric(substr(end[1], 1, 4)), 01), 
-                              nd = c(as.numeric(substr(end[2], 1, 4)), 12))
-    
-    return(data.frame("station" = j, "month" = month.abb, 
-                      "st" = tst.st, "nd" = tst.nd))  
+    Reduce(function(...) merge(..., by = c(1, 2)), 
+           foreach(k = start, l = end) %do% {
+      tmp <- read.csv(i, stringsAsFactors = FALSE)
+      tmp$year <- as.Date(substr(tmp$Datetime, 1, 10))
+      tmp$yearmon <- as.yearmon(tmp$year)
+      
+      tmp.st <- subset(tmp, year >= as.Date(k[1]) & year <= as.Date(k[2]))
+      tmp.st.agg <- aggregate(tmp.st[, c("TEMP", "MAX", "MIN")], 
+                                     by = list(tmp.st$yearmon), FUN = mean)
+      
+      tmp.nd <- subset(tmp, year >= as.Date(l[1]) & year <= as.Date(l[2]))
+      tmp.nd.agg <- aggregate(tmp.nd[, c("TEMP", "MAX", "MIN")], 
+                                     by = list(tmp.nd$yearmon), FUN = mean)
+      
+      tst.st <- vectorHarmonics(tmp.st.agg$TEMP, frq = 12, fun = mean, m = 2,
+                                st = c(as.numeric(substr(k[1], 1, 4)), 01), 
+                                nd = c(as.numeric(substr(k[2], 1, 4)), 12))
+      tst.nd <- vectorHarmonics(tmp.nd.agg$TEMP, frq = 12, fun = mean, m = 2,
+                                st = c(as.numeric(substr(l[1], 1, 4)), 01), 
+                                nd = c(as.numeric(substr(l[2], 1, 4)), 12))
+      
+      tmp.df <- data.frame("station" = j, 
+                           "month" = month.abb, 
+                           "st" = tst.st, 
+                           "nd" = tst.nd)
+      
+      index <- c(grep("^st$", names(tmp.df)), grep("^nd$", names(tmp.df)))
+      names(tmp.df)[index] <- 
+        c(paste(substr(k, 1, 4), collapse = "-"), 
+          paste(substr(l, 1, 4), collapse = "-"))
+      
+      return(tmp.df)
+    })
     
   }
   
   # Reformat data
-  tmp.all <- melt(tmp.all)
+  tmp.all <- melt(tmp.all, id.vars = 1:2)
   # Reorder factor levels of 'month' column
   tmp.all$month <- factor(tmp.all$month, levels = month.abb)
   
   
   ## ggplot
+
+  if (is.list(start) & is.list(end)) {
+    
+    label.st.1 <- paste(substr(start[[1]], 1, 4), collapse = "-")
+    label.st.2 <- paste(substr(start[[2]], 1, 4), collapse = "-")
+    
+    label.nd.1 <- paste(substr(end[[1]], 1, 4), collapse = "-")
+    label.nd.2 <- paste(substr(end[[2]], 1, 4), collapse = "-")
+    
+    cols <- palette
+    names(cols) <- c(label.st.1, label.nd.1, label.st.2, label.nd.2)
+    
+    ggplot(aes(x = month, y = value, group = variable, colour = variable), 
+           data = tmp.all) + 
+      geom_line(lwd = 1) + 
+      facet_wrap(~ station, ncol = ncol) + 
+      scale_colour_manual("", 
+                          values = cols, 
+                          breaks = c(label.st.2, label.st.1, label.nd.1, label.nd.2)) + 
+      labs(list(x = "\nMonth", y = "Temperature [°C]\n")) + 
+      theme_bw() + 
+      theme(text = element_text(...), 
+            legend.key = element_rect(fill = "transparent"), 
+            panel.grid.major = element_line(size = 1.2), 
+            panel.grid.minor = element_line(size = 1.1))
+  } else {
+
+    label.st <- paste(substr(start, 1, 4), collapse = "-")
+    label.nd <- paste(substr(end, 1, 4), collapse = "-")
+    
+    ggplot(aes(x = month, y = value, colour = variable, group = variable), 
+           data = tmp.all) + 
+      geom_line(lwd = 1) + 
+      facet_wrap(~ station, ncol = ncol) + 
+      scale_colour_manual("", values = c("cornflowerblue", "red2"), 
+                          labels = c(label.st.1, label.nd.1)) + 
+      labs(list(x = "\nMonth", y = "Temperature [°C]\n")) + 
+      theme_bw() + 
+      theme(text = element_text(...), 
+            legend.key = element_rect(fill = "transparent"), 
+            panel.grid.major = element_line(size = 1.2), 
+            panel.grid.minor = element_line(size = 1.1))
   
-  ggplot(aes(x = month, y = value, colour = variable, group = variable), 
-         data = tmp.all) + 
-    geom_line(lwd = 1) + 
-    facet_wrap(~ station, ncol = 2) + 
-    scale_colour_manual("", values = c("cornflowerblue", "red2"), 
-                        labels = c("1973-1977", "2008-2012")) + 
-    labs(list(x = "\nMonth", y = "Temperature [°C]\n")) + 
-    theme_bw() + 
-    theme(text = element_text(...), 
-          legend.key = element_rect(fill = "transparent"), 
-          panel.grid.major = element_line(size = 1.2), 
-          panel.grid.minor = element_line(size = 1.1))
+  }
   
 }
