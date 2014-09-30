12cfec193ee2c10be53eb9d3765c19b70e2bca75
fdetsch <florian.detsch@staff.uni-marburg.de>
2014-09-30 17:05:17 +0000

    Major update: added documentation to function `toCelsius()`.

--- R/toCelsius.R
+++ R/toCelsius.R
@@ -1,6 +1,23 @@
-#' Convert °F to °C
+#' Convert degree Fahrenheit to degree Celsius
+#' 
+#' @description
+#' This function converts temperature values from native degree Fahrenheit to 
+#' degree Celsius.
+#' 
+#' @param val Numeric. A vector containing temperature values in degree Fahrenheit.
+#' @param ... Additional arguments passed to \code{\link{round}}.
+#' 
+#' @return
+#' A numeric vector containing temperature values in degree Celsius.
+#' 
+#' @author
+#' Florian Detsch
+#' 
+#' @examples
+#' toCelsius(val = c(68, 72, 76), digits = 1)
 #' 
 #' @export toCelsius
+#' @aliases toCelsius
 toCelsius <- function(val, 
                       ...) {
   
