49ecbe77b139ac0135eaaf4ab74fb2d7fcd5bd99
fdetsch <florian.detsch@staff.uni-marburg.de>
2014-10-01 11:32:16 +0200

    Major update: added documentation to `dlGsodStations()`, implemented `dplyr` filtering and changed variable names.

--- R/dlGsodStations.R
+++ R/dlGsodStations.R
@@ -1,68 +1,113 @@
-#' Download selected GSOD data based on supplied station ID
+#' Download (and extract) GSOD data
+#' 
+#' @description
+#' Download (and extract) data from a GSOD station based on its unique USAF code.
+#' 
+#' @param usaf Numeric. A unique USAF station code. It can either be manually 
+#' determined from \code{data(gsodstations)}, or retreived from spatial 
+#' subsetting (see \code{\link{stationFromCoords}}, 
+#' \code{\link{stationFromExtent}}).
+#' @param start_year Numeric. The desired year to start data acquisition. If not 
+#' supplied, download starts from the first year of measurement. 
+#' @param end_year Numeric. The desired year to stop data acquisition. If not 
+#' supplied, download stops at the last year of measurement. 
+#' @param dsn Character, default is the current working directory. Destination 
+#' folder for data download. 
+#' @param unzip Logical, default is FALSE. If TRUE, *.gz files are unpacked and 
+#' merged after download. 
+#' @param rm_gz Logical, default is FALSE. If TRUE, *.gz files are removed after
+#' extraction. If \code{unzip = FALSE}, this argument is ignored. 
+#' @param ... Additional arguments passed to \code{\link{gzGsodStations}}.
+#' 
+#' @return
+#' A vector of *.gz filenames, or an object of class \code{data.frame} if 
+#' \code{unzip = TRUE}.
+#' 
+#' @author
+#' Florian Detsch
+#' 
+#' @examples
+#' library(dplyr)
+#' 
+#' data(gsodstations)
+#' moshi <- filter(gsodstations, STATION.NAME == "MOSHI")
+#' 
+#' # Download data from Moshi, Tanzania, from 1990 to 1995
+#' gsod_moshi <- dlGsodStations(usaf = moshi$USAF, 
+#'                              start_year = 1990, end_year = 1995, 
+#'                              dsn = paste0(getwd(), "/data/moshi/"), 
+#'                              unzip = TRUE)
+#' 
+#' # Plot temperature data (but: time series not continuous!)                                                         
+#' plot(gsod_moshi$TEMP, type = "l")
 #' 
 #' @export dlGsodStations
+#' @aliases dlGsodStations
 dlGsodStations <- function(usaf,
-                           start.year.rec, 
-                           end.year.rec,
+                           start_year = NA, 
+                           end_year = NA,
                            dsn = ".", 
-                           unzip = TRUE, 
-                           remove.gz = FALSE,
-                           remove.op = FALSE,
+                           unzip = FALSE, 
+                           rm_gz = FALSE,
                            ...) {
   
+  rm_gz <- ifelse(unzip, rm_gz, FALSE)
+  
   # Load GSOD station list from scratch if not supplied
+  data(gsodstations)
   locations <- gsodstations
   
   # Extract desired station from list of GSOD stations
-  dl.usaf <- locations[locations$USAF %in% usaf, ]
+  dl_usaf <- locations %>% filter(USAF == usaf)
 
-  # Loop through all GSOD stations to be downloaded
-  for (usafs in 1:nrow(dl.usaf)) {
-    
-    # Verify user-defined temporal range
-    start.year <- max(start.year.rec, substr(dl.usaf$BEGIN[usafs], 1, 4))
-    end.year <- min(end.year.rec, substr(dl.usaf$END[usafs], 1, 4))
+  # If not supplied, set start_year and end_year to temporal range of
+  # available measurements
+  start_measurement <- as.numeric(substr(dl_usaf$BEGIN, 1, 4))
+  end_measurement <- as.numeric(substr(dl_usaf$END, 1, 4))
 
-    # Abandon current iteration in case start_year is higher than end_year
-    if (start.year > end.year) {
-      cat("Skipping GSOD station ", dl.usaf$USAF[usafs], 
-          ": Start year is higher than end year!", sep = "")
-      next()
-    }
+  if (is.na(start_year))
+    start_year <- start_measurement
+  if (is.na(end_year))
+    end_year <- end_measurement
+  
+  # Else, verify user-defined temporal range
+  start_year <- max(start_year, start_measurement)
+  end_year <- min(end_year, end_measurement)
+  
+  if (start_year > end_year)
+    stop("Skipping GSOD station ", dl_usaf$USAF, 
+         ": Start year is higher than end year!", sep = "")
+  
+  cat("Processing GSOD station", dl_usaf$USAF, "... \n")
+  
+  # Download op.gz of current station per year
+  fls_gz <- sapply(start_year:end_year, function(year) {
+    # Basename of both URL and destfile
+    dlbase <- paste0(dl_usaf$USAF, "-", dl_usaf$WBAN, "-", year, ".op.gz")
+    # URL
+    dlurl <- paste0("ftp://ftp.ncdc.noaa.gov/pub/data/gsod/", year, "/", 
+                    dlbase)
+    # Destfile
+    dlfile <- paste0(dsn, "/", dlbase)
+    # Download
+    try(download.file(dlurl, dlfile), silent = FALSE)
     
-    cat("Processing GSOD station", dl.usaf$USAF[usafs], "... \n")
-    
-    # Download op.gz of current station per year
-    for (year in start.year:end.year) {
-      # Basename of both URL and destfile
-      dlbase <- paste(dl.usaf$USAF[usafs], "-", dl.usaf$WBAN[usafs], "-", 
-                      year, ".op.gz", sep = "")
-      # URL
-      dlurl <- paste0("ftp://ftp.ncdc.noaa.gov/pub/data/gsod/", year, "/", 
-                      dlbase)
-      # Destfile
-      dlfile <- paste0(dsn, "/", dlbase)
-      # Download
-      print(dlurl)
-      try(download.file(dlurl, dlfile), silent = TRUE)
-    }
-  }
+    return(dlfile)
+  })
     
   # Unzip downloaded files (optional)
   if (unzip) {
     
-    jnk <- gzGsodStations(dsn = dsn, 
-                          start.year = start.year, 
-                          end.year = end.year, 
-                          ...)
-    # List and return unzipped .op files
-    files <- list.files(dsn, pattern = ".*.csv$", full.names = TRUE)
-    return(files)
+    df <- gzGsodStations(usaf = usaf, 
+                         dsn = dsn, 
+                         start_year = start_year, 
+                         end_year = end_year, 
+                         ...)
+    return(df)
     
   } else {
     
-    # List and return downloaded .gz files
-    files <- list.files(dsn, pattern = ".*.op.gz$", full.names = TRUE)
-    return(files)
+    # Return downloaded .gz files
+    return(fls_gz)
   }
 }
