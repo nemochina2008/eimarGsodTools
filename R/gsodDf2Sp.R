6019347ef2762b4999a7459d4e1a355170a22f99
fdetsch <florian.detsch@staff.uni-marburg.de>
2014-10-01 12:23:52 +0200

    Major update: added documentation to `gsodDf2Sp()`.

--- R/gsodDf2Sp.R
+++ R/gsodDf2Sp.R
@@ -1,6 +1,32 @@
-#' Convert GSOD station list to sp::SpatialPolygonsDataFrame
+#' Convert GSOD station list to spatial object
+#' 
+#' @description
+#' This function converts the list of available GSOD stations from \code{data.frame}
+#' to \code{SpatialPointsDataFrame}. 
+#' 
+#' @param data \code{data.frame}. Usually \code{data("gsodstations")}.
+#' @param ... Additional arguments. Currently not in use.
+#' 
+#' @return
+#' An object of class \code{SpatialPointsDataFrame}.
+#' 
+#' @author
+#' Florian Detsch
+#' 
+#' @examples
+#' # Load data
+#' data(gsodstations)
+#' 
+#' # Reformat station list
+#' gsod_df <- gsodReformat(gsodstations, df2sp = FALSE)
+#' 
+#' # Convert station list to spatial object
+#' gsod_shp <- gsodDf2Sp(data = gsod_df)
+#'                          
+#' plot(gsod_shp)                        
 #' 
 #' @export gsodDf2Sp
+#' @aliases gsodDf2Sp
 gsodDf2Sp <- function(data, 
                       ...) {
 
