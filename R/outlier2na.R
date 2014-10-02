ea1a48957c3212e31a3dedd8b22b485f58be213a
fdetsch <florian.detsch@staff.uni-marburg.de>
2014-10-02 10:59:22 +0200

    Major update: added documentation to `outlier2na()`.

--- R/outlier2na.R
+++ R/outlier2na.R
@@ -1,36 +1,48 @@
-#' Replace statistical outliers with NA
+#' Remove statistical outliers from time-series values
 #' 
+#' @description
+#' This is a wrapper function around \code{\link{tsOutliers}} that automatically
+#' removes identified statistical outliers from a measurement series.
+#' 
+#' @param val Numeric. A vector of observed time-series values. 
+#' @param lower_quantile Numeric, default is 0.2. The lower quantile limit. 
+#' @param upper_quantile Numeric, default is 0.8. The upper quantile limit.
+#' @param ... Additional arguments passed to \code{\link{tsOutliers}}.
+#' 
+#' @return
+#' An outlier-cleaned numeric vector of time-series values.
+#' 
+#' @author
+#' Florian Detsch
+#' 
+#' @seealso 
+#' \code{\link{tsOutliers}}
+#' 
+#' @examples
+#' # Random time-series values
+#' set.seed(10)
+#' x <- rnorm(100, 0, 2)
+#' 
+#' # Replace outliers with NA
+#' x2 <- outlier2na(x, lower_quantile = .35, upper_quantile = .7)
+#' 
+#' plot(x, type = "l", lty = 2)
+#' lines(x2, col = "red")
+#'
 #' @export outlier2na
-outlier2na <- function(data, 
-                       prm = "TEMP", 
-                       save.output = F,
+#' @aliases outlier2na
+outlier2na <- function(val, 
+                       lower_quantile = .2, 
+                       upper_quantile = .8,
                        ...) {
   
-  # Loop through all provided parameters
-  for (j in prm) {
-    # Extract time series of current parameter from ki.data object
-    vec <- data@Parameter[[j]]
-    # Identify outliers
-    outliers <- tsOutliers(vec, index = T)
-    # Replace identified outliers with NA
-    vec[outliers] <- NA
-    # Insert adjusted time series into referring slot of ki.data object
-    data@Parameter[[j]] <- vec
-  }
+  # Identify outliers
+  outliers <- tsOutliers(val, index = TRUE, 
+                         lower_quantile = lower_quantile, 
+                         upper_quantile = upper_quantile, 
+                         ...)
+  # Replace identified outliers with NA
+  val[outliers] <- NA
   
-  # Save (optional) and return spike-filtered data set
-  if (save.output)
-    write.csv(data.frame("Datetime" = data@Datetime, 
-                         "Timezone" = data@Timezone, 
-                         "Aggregationtime" = data@Aggregationtime, 
-                         "PlotId" = data@PlotId$Shortname, 
-                         "EpPlotId" = data@EpPlotId, 
-                         "StationId" = data@StationId$Shortname, 
-                         "Processlevel" = data@Processlevel, 
-                         "Qualityflag" = data@Qualityflag, 
-                         "TEMP" = data@Parameter$TEMP, 
-                         "MAX" = data@Parameter$MAX, 
-                         "MIN" = data@Parameter$MIN), ...)
-  
-  return(data)
+  return(val)
 }
