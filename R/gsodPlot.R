5159acfc59c37e27e9165c2056443fdab0c015c4
fdetsch <florian.detsch@staff.uni-marburg.de>
2014-10-06 16:29:38 +0200

    Major update: added documentation to `gsodPlot()`.

--- R/gsodPlot.R
+++ R/gsodPlot.R
@@ -1,24 +1,71 @@
-#' Function to plot gap-filled GSOD data sets
+#' Plot GSOD data
 #' 
+#' @description
+#' This function aims at visualizing GSOD data, both raw/adjusted for outliers and 
+#' imputed, as well as linear trends of mean, minimum and maximum daily air
+#' temperature.
+#' 
+#' @param fls_orig Character, default is NULL. Filepath(s) to the initial (or 
+#' outlier-adjusted) GSOD data set(s). 
+#' @param fls Character. Filepath(s) to the imputed GSOD data set(s).
+#' @param stations Character. Name(s) of the station(s) corresponding to \code{fls}
+#' and \code{fls_orig} that will be displayed above each facet.
+#' @param prm Character, default is "TEMP". Determines which parameter to
+#' visualize. If \code{type = "trends"}, this argument will be ignored, and 
+#' columns "TEMP", "MIN" and "MAX" must be available in \code{fls} and 
+#' \code{fls_orig}.
+#' @param ... Additional arguments passed to \code{\link{element_text}}.
+#' 
+#' @return
+#' An object of class \code{ggplot}.
+#' 
+#' @author
+#' Florian Detsch
+#' 
+#' @examples
+#' library(dplyr)
+#' 
+#' data(gsodstations)
+#' gar <- filter(gsodstations, STATION.NAME == "GARISSA")
+#' 
+#' gsod_gar <- dlGsodStations(usaf = gar$USAF,
+#'                            start_year = 1990, end_year = 1995,
+#'                            dsn = paste0(getwd(), "/data/gar/"),
+#'                            unzip = TRUE)
+#' 
+#' # Conversion to KiLi SP1 `ki.data` object
+#' ki_gar <- gsod2ki(data = gsod_gar,
+#'                   prm_col = c("TEMP", "MIN", "MAX"),
+#'                   df2ki = TRUE)
+#' 
+#' # Fill small gaps (n <= 5) with linear interpolation
+#' ki_gar_linint <- gfLinInt(data = ki_gar, 
+#'                           prm = c("TEMP", "MIN", "MAX"))
+#' 
+#' # Fill remaining gaps based on SSA
+#' ki_gar_ssa <- gfSsa(data = ki_gar_linint, 
+#'                     prm = c("TEMP", "MIN", "MAX"), 
+#'                     reversed_forecast = FALSE, 
+#'                     digits = 2)
+#' 
+#' plot(slot(ki_gar_ssa, "Parameter")[["TEMP"]], type = "l", col = "red")
+#' lines(slot(ki_gar_linint, "Parameter")[["TEMP"]], col = "green")
+#' lines(slot(ki_gar, "Parameter")[["TEMP"]])             
+#'
 #' @export gsodPlot
-gsodPlot <- function(fls.orig = NULL,
-                     fls.gf, 
+#' @aliases gsodPlot
+gsodPlot <- function(fls_orig = NULL,
+                     fls, 
                      stations, 
-                     prm.orig = "TEMP",
+                     prm = "TEMP",
                      type = "trends",
                      ...) {
-  
-  # Required packages
-  libs <- c("zoo", "doParallel", "Hmisc", "bfast", "ggplot2", "scales", 
-            "plyr", "reshape2")
-  lapply(libs, function(i) require(i, character.only = TRUE))
-  
   
   ## Data import
   
   # Initial GSOD data
-  if (!is.null(fls.orig)) {
-    ta.orig <- lapply(fls.orig, function(i) {
+  if (!is.null(fls_orig)) {
+    ta.orig <- lapply(fls_orig, function(i) {
       tmp.ts <- read.zoo(i, format = "%Y-%m-%d %H:%M:%S", 
                          header = TRUE, sep = ",", regular = TRUE)
       frequency(tmp.ts) <- 365
@@ -27,170 +74,115 @@ gsodPlot <- function(fls.orig = NULL,
     
     # Reformat and append data
     ta.orig.df <- foreach(i = ta.orig, j = stations, .combine = "rbind") %do%
-      data.frame(DATE = time(i), PLOT = j, Original = as.numeric(i[, prm.orig]))
+      data.frame(DATE = time(i), PLOT = j, Original = as.numeric(i[, prm]))
   }
   
   # Gap-filled GSOD data
-  if (!is.null(fls.gf)) {
-    ta.gf <- lapply(fls.gf, function(i) {
-      ta.gf.ts <- read.zoo(i, format = "%Y-%m-%d %H:%M:%S", 
-                           header = TRUE, sep = ",", regular = TRUE)
-      return(ta.gf.ts)
-    })
+  ta.gf <- lapply(fls, function(i) {
+    ta.gf.ts <- read.zoo(i, format = "%Y-%m-%d %H:%M:%S", 
+                         header = TRUE, sep = ",", regular = TRUE)
+    return(ta.gf.ts)
+  })
+  
+  
+  ## Plotting stuff
+  
+  # Start and end date 
+  start_year <- substr(ta.orig.df$DATE[1], 1, 4)
+  start_date <- as.Date(paste0(start_year, "-01-01"))
+  
+  end_year <- substr(ta.orig.df$DATE[nrow(ta.orig.df)], 1, 4)
+  end_date <- as.Date(paste0(end_year, "-12-31"))
+  
+  # Plot original and gap-filled GSOD data 
+  if (type == "original") {
+        
+    ggplot(aes(x = DATE, y = Original), data = ta.orig.df) + 
+      geom_line() + 
+      facet_wrap(~ PLOT, ncol = 2) + 
+      scale_x_date(limits = c(start_date, end_date), 
+                   breaks = seq(start_date, end_date, "2 years"), 
+                   labels = date_format("%Y"), minor_breaks = date_breaks("1 year")) + 
+      theme_bw() + 
+      theme(text = element_text(...), 
+            legend.key = element_rect(fill = "transparent"), 
+            panel.grid.major = element_line(size = 1.2), 
+            panel.grid.minor = element_line(size = 1.1))
     
+  } else if (type == "both") {
+    # Reformat and append gap-filled station data
+    ta.gf.df <- foreach(i = ta.gf, j = stations, .combine = "rbind") %do%
+      data.frame(DATE = time(i), PLOT = j, Imputed = as.numeric(i[, prm]))
     
-    # ## Breakpoint detection
-    # 
-    # # Loop through each time series
-    # tmp.bfast <- foreach(i = tmp, .packages = lib) %dopar% {
-    #   tmp.ts <- ts(na.exclude(as.numeric(i[, 8])), 
-    #                start = 1973, end = 2013, frequency = 365)
-    #   bfast(tmp.ts, season = "harmonic", max.iter = 1, breaks = 5)
-    # }
+    # Merge and meltoriginal and gap-filled data
+    ta.orig.gf.df <- merge(ta.orig.df, ta.gf.df, all = TRUE, by = c(1, 2))
+    ta.orig.gf.df.mlt <- melt(ta.orig.gf.df, id.vars = c(1, 2))
     
+    ta.orig.gf.df.mlt$variable <- factor(ta.orig.gf.df.mlt$variable, 
+                                         levels = c("Imputed", "Original"))
     
-    ## Plotting stuff
+    ggplot(aes(x = DATE, y = value, group = variable, colour = variable), 
+           data = ta.orig.gf.df.mlt) + 
+      geom_line() + 
+      facet_wrap(~ PLOT, ncol = 2) + 
+      scale_x_date(limits = c(start_date, end_date), 
+                   breaks = seq(start_date, end_date, "2 years"), 
+                   labels = date_format("%Y"), minor_breaks = date_breaks("1 year")) + 
+      scale_colour_manual("", values = c("grey65", "black"), 
+                          labels = c("Original data", "Imputed data"), 
+                          breaks = c("Original", "Imputed")) +
+      theme_bw() + 
+      theme(text = element_text(...), 
+            legend.key = element_rect(fill = "transparent"), 
+            panel.grid.major = element_line(size = 1.2), 
+            panel.grid.minor = element_line(size = 1.1))
+        
+    # Plot gap-filled GSOD data only  
+  } else if (type == "trends") {  
     
-    # Plot original and gap-filled GSOD data 
-    if (type == "original") {
-      
-      #     # Reformat and append gap-filled station data
-      #     ta.gf.df <- foreach(i = ta.gf, j = stations, .combine = "rbind") %do%
-      #       data.frame(DATE = time(i), PLOT = j, Imputed = as.numeric(i[, prm.orig]))
-      
-#       # Merge and melt original and gap-filled data
-#       ta.orig.gf.df <- merge(ta.orig.df, ta.gf.df, all = TRUE, by = c(1, 2))
-#       ta.orig.gf.df.mlt <- melt(ta.orig.gf.df, id.vars = c(1, 2))
-#       
-#       ta.orig.gf.df.mlt$variable <- factor(ta.orig.gf.df.mlt$variable, 
-#                                            levels = c("Imputed", "Original"))
-      
-      ggplot(aes(x = DATE, y = Original), data = ta.orig.df) + 
-        geom_line() + 
-        facet_wrap(~ PLOT, ncol = 2) + 
-        scale_x_date(limits = c(as.Date("1973-01-01"), as.Date("2013-12-31")), 
-                     breaks = seq(as.Date("1970-01-01"), as.Date("2020-01-01"), "10 years"), 
-                     labels = date_format("%Y"), minor_breaks = date_breaks("2 years")) + 
-        #       scale_colour_manual("", values = c("grey65", "black"), 
-        #                           labels = c("Original data", "Imputed data"), 
-        #                           breaks = c("Original", "Imputed")) +
-        labs(list(title = "GSOD daily air temperature (1973-2012)\n", 
-                  x = "\nTime [d]", y = "Temperature [°C]\n")) + 
-        theme_bw() + 
-        theme(text = element_text(...), 
-              legend.key = element_rect(fill = "transparent"), 
-              panel.grid.major = element_line(size = 1.2), 
-              panel.grid.minor = element_line(size = 1.1))
-      
-    } else if (type == "both") {
-      # Reformat and append gap-filled station data
-      ta.gf.df <- foreach(i = ta.gf, j = stations, .combine = "rbind") %do%
-        data.frame(DATE = time(i), PLOT = j, Imputed = as.numeric(i[, prm.orig]))
-      
-      # Merge and meltoriginal and gap-filled data
-      ta.orig.gf.df <- merge(ta.orig.df, ta.gf.df, all = TRUE, by = c(1, 2))
-      ta.orig.gf.df.mlt <- melt(ta.orig.gf.df, id.vars = c(1, 2))
-      
-      ta.orig.gf.df.mlt$variable <- factor(ta.orig.gf.df.mlt$variable, 
-                                           levels = c("Imputed", "Original"))
-      
-      ggplot(aes(x = DATE, y = value, group = variable, colour = variable), 
-             data = ta.orig.gf.df.mlt) + 
-        geom_line() + 
-        facet_wrap(~ PLOT, ncol = 2) + 
-        scale_x_date(limits = c(as.Date("1973-01-01"), as.Date("2013-12-31")), 
-                     breaks = seq(as.Date("1970-01-01"), as.Date("2020-01-01"), "10 years"), 
-                     labels = date_format("%Y"), minor_breaks = date_breaks("2 years")) + 
-        scale_colour_manual("", values = c("grey65", "black"), 
-                            labels = c("Original data", "Imputed data"), 
-                            breaks = c("Original", "Imputed")) +
-        labs(list(title = "GSOD daily air temperature (1973-2012)\n", 
-                  x = "\nTime [d]", y = "Temperature [°C]\n")) + 
-        theme_bw() + 
-        theme(text = element_text(...), 
-              legend.key = element_rect(fill = "transparent"), 
-              panel.grid.major = element_line(size = 1.2), 
-              panel.grid.minor = element_line(size = 1.1))
-      
-    } else if (type == "all.in.one") {
-      # Reformat and append gap-filled station data
-      ta.gf.df <- foreach(i = ta.gf, j = stations, .combine = "rbind") %do%
-        data.frame(DATE = time(i), PLOT = j, Imputed = as.numeric(i[, prm.orig]))
-      
-      # Merge and meltoriginal and gap-filled data
-      ta.orig.gf.df <- merge(ta.orig.df, ta.gf.df, all = TRUE, by = c(1, 2))
-      ta.orig.gf.df.mlt <- melt(ta.orig.gf.df, id.vars = c(1, 2))
-      
-      ta.orig.gf.df.mlt$variable <- factor(ta.orig.gf.df.mlt$variable, 
-                                           levels = c("Imputed", "Original"))
-      
-      ggplot(aes(x = DATE, y = value, group = variable, colour = variable), 
-             data = ta.orig.gf.df.mlt) + 
-        geom_line() + 
-        facet_wrap(~ PLOT, ncol = 2) + 
-        scale_x_date(limits = c(as.Date("1973-01-01"), as.Date("2013-12-31")), 
-                     breaks = seq(as.Date("1970-01-01"), as.Date("2020-01-01"), "10 years"), 
-                     labels = date_format("%Y"), minor_breaks = date_breaks("2 years")) + 
-        scale_colour_manual("", values = c("grey65", "black"), 
-                            labels = c("Original data", "Imputed data"), 
-                            breaks = c("Original", "Imputed")) +
-        labs(list(title = "GSOD daily air temperature (1973-2012)\n", 
-                  x = "\nTime [d]", y = "Temperature [°C]\n")) + 
-        theme_bw() + 
-        theme(text = element_text(...), 
-              legend.key = element_rect(fill = "transparent"), 
-              panel.grid.major = element_line(size = 1.2), 
-              panel.grid.minor = element_line(size = 1.1))
-      
-      # Plot gap-filled GSOD data only  
-    } else if (type == "trends") {  
-
-      # Reformat, append and melt quality-controlled data
-      ta.orig.df <- melt(foreach(i = ta.orig, j = stations, .combine = "rbind") %do%
-        data.frame(DATE = time(i), PLOT = j, MEAN = as.numeric(i$TEMP), 
-                   MAX = as.numeric(i$MAX), MIN = as.numeric(i$MIN)), 
-        id.vars = c(1, 2))
-
-      # Reformat, append and melt gap-filled data
-      ta.gf.df <- melt(foreach(i = ta.gf, j = stations, .combine = "rbind") %do%
-        data.frame(DATE = time(i), PLOT = j, MEAN = as.numeric(i$TEMP), 
-                   MAX = as.numeric(i$MAX), MIN = as.numeric(i$MIN)), 
-        id.vars = c(1, 2))
-      
-      # Reorder factor levels
-      ta.orig.df$variable <- factor(ta.orig.df$variable, levels = c("MIN", "MEAN", "MAX"))
-      ta.gf.df$variable <- factor(ta.gf.df$variable, levels = c("MIN", "MEAN", "MAX"))
-      
-      ggplot(aes(x = DATE, y = value, colour = variable, linetype = variable), 
-             data = ta.gf.df) + 
-        geom_line(,subset = .(variable == "MEAN"), colour = "grey65") +
-        geom_line(aes(x = DATE, y = value, colour = variable, linetype = variable),
-                  data = ta.orig.df, ,subset = .(variable == "MEAN"), 
-                  colour = "grey35") +
-        stat_smooth(size = 1.2, method = "lm", se = FALSE) + 
-        facet_wrap(~ PLOT, ncol = 1) + 
-        scale_x_date(limits = c(as.Date("1973-01-01"), as.Date("2013-12-31")), 
-                     breaks = seq(as.Date("1970-01-01"), as.Date("2020-01-01"), "10 years"), 
-                     labels = date_format("%Y"), minor_breaks = date_breaks("2 years")) + 
-        scale_linetype_manual("Linear trends of daily", 
-                              values = c("dotted", "solid", "dotted"), 
-                              labels = c("Minimum", 
-                                         "Mean",                                      
-                                         "Maximum")) +
-        scale_colour_manual("Linear trends of daily", 
-                            values = c("blue", "black", "red"), 
+    # Reformat, append and melt quality-controlled data
+    ta.orig.df <- melt(foreach(i = ta.orig, j = stations, .combine = "rbind") %do%
+                         data.frame(DATE = time(i), PLOT = j, MEAN = as.numeric(i$TEMP), 
+                                    MAX = as.numeric(i$MAX), MIN = as.numeric(i$MIN)), 
+                       id.vars = c(1, 2))
+    
+    # Reformat, append and melt gap-filled data
+    ta.gf.df <- melt(foreach(i = ta.gf, j = stations, .combine = "rbind") %do%
+                       data.frame(DATE = time(i), PLOT = j, MEAN = as.numeric(i$TEMP), 
+                                  MAX = as.numeric(i$MAX), MIN = as.numeric(i$MIN)), 
+                     id.vars = c(1, 2))
+    
+    # Reorder factor levels
+    ta.orig.df$variable <- factor(ta.orig.df$variable, levels = c("MIN", "MEAN", "MAX"))
+    ta.gf.df$variable <- factor(ta.gf.df$variable, levels = c("MIN", "MEAN", "MAX"))
+    
+    ggplot(aes(x = DATE, y = value, colour = variable, linetype = variable), 
+           data = ta.gf.df) + 
+      geom_line(,subset = .(variable == "MEAN"), colour = "grey65") +
+      geom_line(aes(x = DATE, y = value, colour = variable, linetype = variable),
+                data = ta.orig.df, ,subset = .(variable == "MEAN"), 
+                colour = "grey35") +
+      stat_smooth(size = 1.2, method = "lm", se = FALSE) + 
+      facet_wrap(~ PLOT, ncol = 1) + 
+      scale_x_date(limits = c(start_date, end_date), 
+                   breaks = seq(start_date, end_date, "2 years"), 
+                   labels = date_format("%Y"), minor_breaks = date_breaks("1 year")) + 
+      scale_linetype_manual("Linear trends of daily", 
+                            values = c("dotted", "solid", "dotted"), 
                             labels = c("Minimum", 
                                        "Mean",                                      
                                        "Maximum")) +
-        labs(colour = "Linear trends of daily", 
-             linetype = "Linear trends of daily") + 
-        labs(list(title = "GSOD daily air temperature (1973-2012)\n", 
-                  x = "\nTime [d]", y = "Temperature [°C]\n")) + 
-        theme_bw() + 
-        theme(text = element_text(...), 
-              legend.key = element_rect(fill = "transparent"), 
-              panel.grid.major = element_line(size = 1.2), 
-              panel.grid.minor = element_line(size = 1.1))
-    }
+      scale_colour_manual("Linear trends of daily", 
+                          values = c("blue", "black", "red"), 
+                          labels = c("Minimum", 
+                                     "Mean",                                      
+                                     "Maximum")) +
+      labs(colour = "Linear trends of daily", 
+           linetype = "Linear trends of daily") + 
+      theme_bw() + 
+      theme(text = element_text(...), 
+            legend.key = element_rect(fill = "transparent"), 
+            panel.grid.major = element_line(size = 1.2), 
+            panel.grid.minor = element_line(size = 1.1))
   }
 }
