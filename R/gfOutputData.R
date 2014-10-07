4c0e2e6efdccf32d674393b706d95c64d2481bce
fdetsch <florian.detsch@staff.uni-marburg.de>
2014-10-07 17:00:43 +0200

    Major update: added documentation to `gfOutputData()`.

--- R/gfOutputData.R
+++ R/gfOutputData.R
@@ -1,8 +1,27 @@
-#' Julendat function to generate gap-filled output data
+#' Julendat function to convert \code{ki.data} to \code{data.frame}
+#' 
+#' @description
+#' This function converts objects of class \code{ki.data} to "ordinary" objects
+#' of class \code{data.frame} that follow KiLi SP1 standard formatting conventions. 
+#' 
+#' @param data.dep Object of class \code{ki.data}. 
+#' @param plevel, default is "NA". Processing level. Not required for GSOD data
+#' processing. 
+#' @param ... Further arguments. Currently not in use. 
+#' 
+#' @return
+#' A \code{data.frame}. 
+#' 
+#' @author
+#' Florian Detsch
+#' 
+#' @seealso
+#' \code{\link{gsod2ki}}, \code{\link{as.ki.data}}
 #' 
 #' @export gfOutputData
+#' @aliases gfOutputData
 gfOutputData <- function(data.dep,
-                         plevel, 
+                         plevel = "NA", 
                          ...) {
   
   # Define columns of output data frame
